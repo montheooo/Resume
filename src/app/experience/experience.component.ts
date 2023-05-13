7abced633da69a97c7297c0a95e3663719e7e6d8
montheooo <montheooo@yahoo.fr>
2023-05-13 07:29:00 -0400

    experience component property binding

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -10,18 +10,8 @@ import { Experience } from '../Models/Experience';
 export class ExperienceComponent {
 
   @Input()
-  myData: Experience =
+  exp! : Experience ;
 
-  {
 
-      company:"Cascades",
-      position:"Sorter",
-      startDate:"Nov 2022",
-      endDate:"April 2023",
-      location:"Ottawa-Ontario",
-      role:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer",
-      skills:["one", "two", "three", "four"]
-
-    }
 
 }
