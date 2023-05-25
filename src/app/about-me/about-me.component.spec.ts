f9328f4650dced633db34b316d5923a17e677b48
montheooo <montheooo@yahoo.fr>
2023-05-25 11:19:46 -0400

    update tests aboutMe contact-detail footer compone

--- src/app/about-me/about-me.component.spec.ts
+++ src/app/about-me/about-me.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AboutMeComponent } from './about-me.component';
+import { Profile } from '../Models/Profile';
 
 describe('AboutMeComponent', () => {
   let component: AboutMeComponent;
@@ -8,16 +8,34 @@ describe('AboutMeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutMeComponent ]
-    })
-    .compileComponents();
+      declarations: [AboutMeComponent],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(AboutMeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should bind the input property correctly', () => {
+    const profile: Profile = {
+      myName: 'John Doe',
+      myProfession: 'Software Developer',
+      imageProfile: 'profile.jpg',
+      linkedinProfile: 'https://www.linkedin.com/johndoe',
+      githubProfile: 'https://github.com/johndoe',
+      slackProfile: 'johndoe',
+      aboutMe: 'I am a passionate software developer with experience in Angular.',
+    };
+
+    component.about = profile;
+    fixture.detectChanges();
+
+    expect(component.about).toEqual(profile);
+  });
+
 });
